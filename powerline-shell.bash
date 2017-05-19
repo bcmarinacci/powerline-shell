215eda7fe786731c3cac60764943d8a4f2faa310
Benji Marinacci <bcmarinacci@icloud.com>
2017-05-19 00:26:52 -0700

    Remove helper functions

--- powerline-shell.bash
+++ powerline-shell.bash
@@ -19,56 +19,43 @@ __ps_main() {
 
   __ps_repository_status() {
     [ -x $(hash git 2>/dev/null) ] || return
+
     local -r git_en="env LANG=C git"
     local -r branch=$($git_en symbolic-ref --short HEAD 2>/dev/null || $git_en describe --tags --always 2>/dev/null)
 
     [ -n "$branch" ] || return
-    local status_indicators="$__ps_clean_indicator"
-    [ -n "$($git_en status --porcelain)" ] && status_indicators="$__ps_dirty_indicator"
 
-    __ps_num_status() {
-      $git_en status --porcelain | egrep "$1" | wc -l | egrep -o '\d+'
-    }
+    if [ -z "$($git_en status --porcelain)" ]; then
+      local status_indicators="$__ps_clean_indicator"
+    else
+      local status_indicators="$__ps_dirty_indicator"
+    fi
 
-    local -r staged_files=$(__ps_num_status '^[A-Z]')
+    local -r staged_files=$($git_en status --porcelain | egrep '^[A-Z]' | wc -l | egrep -o '\d+')
     [ $staged_files -gt 0 ] && status_indicators+=" $__ps_staged_indicator$staged_files"
-    local -r unstaged_files=$(__ps_num_status '^.[A-Z]')
+    local -r unstaged_files=$($git_en status --porcelain | egrep '^.[A-Z]' | wc -l | egrep -o '\d+')
     [ $unstaged_files -gt 0 ] && status_indicators+=" $__ps_unstaged_indicator$unstaged_files"
-    local -r untracked_files=$(__ps_num_status '^\?\?')
+    local -r untracked_files=$($git_en status --porcelain | egrep '^\?\?' | wc -l | egrep -o '\d+')
     [ $untracked_files -gt 0 ] && status_indicators+=" $__ps_untracked_indicator$untracked_files"
-
-    __ps_num_stash_items() {
-      $git_en stash list | wc -l | egrep -o '\d+'
-    }
-
-    local -r stash_items="$(__ps_num_stash_items)"
+    local -r stash_items=$($git_en stash list | wc -l | egrep -o '\d+')
     [ $stash_items -gt 0 ] && status_indicators+=" $__ps_stash_indicator$stash_items"
-
-    __ps_num_commit_diffs() {
-      $git_en status --branch --porcelain | egrep '^##' | egrep -o "$1 \d+" | egrep -o '\d+'
-    }
-
-    local -r commits_ahead="$(__ps_num_commit_diffs ahead)"
+    local -r commits_ahead=$($git_en status --branch --porcelain | egrep '^##' | egrep -o 'ahead \d+' | egrep -o '\d+')
     [ -n "$commits_ahead" ] && status_indicators+=" $__ps_push_indicator$commits_ahead"
-    local -r commits_behind="$(__ps_num_commit_diffs behind)"
+    local -r commits_behind=$($git_en status --branch --porcelain | egrep '^##' | egrep -o 'behind \d+' | egrep -o '\d+')
     [ -n "$commits_behind" ] && status_indicators+=" $__ps_pull_indicator$commits_behind"
 
     printf " $__ps_branch_indicator  $branch $status_indicators "
   }
 
-  __ps_exit_status() {
-    printf "$1 $__ps_color_reset"
-  }
-
   __ps_ps1() {
     if [ $? -eq 0 ]; then
-      PS1="$(__ps_exit_status $__ps_color_bg_exit_zero)"
+      PS1="$__ps_color_bg_exit_zero "
     else
-      PS1="$(__ps_exit_status $__ps_color_bg_exit_nonzero)"
+      PS1="$__ps_color_bg_exit_nonzero "
     fi
 
-    PS1+="$__ps_color_bg_base$__ps_color_fg_base \w $__ps_color_reset"
-    PS1+="$__ps_color_bg_git$__ps_color_fg_base"
+    PS1+="$__ps_color_reset$__ps_color_bg_base$__ps_color_fg_base \w "
+    PS1+="$__ps_color_reset$__ps_color_bg_git$__ps_color_fg_base"
     # Description: https://github.com/njhartwell/pw3nage
     # Related fix in git-bash: https://github.com/git/git/blob/9d77b0405ce6b471cb5ce3a904368fc25e55643d/contrib/completion/git-prompt.sh#L324
     if shopt -q promptvars; then
