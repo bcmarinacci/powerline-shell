898d59690befa9980d71b9a76cbee962b6bb94f6
Benji Marinacci <benji@buildingconnected.com>
2018-06-14 12:53:30 -0700

    List stash before untracked

--- powerline-shell.bash
+++ powerline-shell.bash
@@ -38,10 +38,10 @@ __ps_main() {
     [[ $staged_files -gt 0 ]] 2>/dev/null && status_indicators+=" $__ps_staged_indicator$staged_files"
     local -r unstaged_files=$(echo "$git_status" | egrep '^.[A-Z]' | wc -l | egrep -o '\d+')
     [[ $unstaged_files -gt 0 ]] 2>/dev/null && status_indicators+=" $__ps_unstaged_indicator$unstaged_files"
-    local -r untracked_files=$(echo "$git_status" | egrep '^\?\?' | wc -l | egrep -o '\d+')
-    [[ $untracked_files -gt 0 ]] 2>/dev/null && status_indicators+=" $__ps_untracked_indicator$untracked_files"
     local -r stash_items=$($git_en stash list | wc -l | egrep -o '\d+')
     [[ $stash_items -gt 0 ]] 2>/dev/null && status_indicators+=" $__ps_stash_indicator$stash_items"
+    local -r untracked_files=$(echo "$git_status" | egrep '^\?\?' | wc -l | egrep -o '\d+')
+    [[ $untracked_files -gt 0 ]] 2>/dev/null && status_indicators+=" $__ps_untracked_indicator$untracked_files"
     local -r git_status_branch=$($git_en status --porcelain --branch)
     local -r commits_ahead=$(echo "$git_status_branch" | egrep '^##' | egrep -o 'ahead \d+' | egrep -o '\d+')
     [[ -n "$commits_ahead" ]] && status_indicators+=" $__ps_push_indicator$commits_ahead"
