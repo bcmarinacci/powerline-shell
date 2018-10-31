9f55a1c945665f0ccd6fea2938955b68841469d6
Benji Marinacci <bcmarinacci@icloud.com>
2018-10-31 00:46:27 -0700

    Use curly braces for variable expansion

--- powerline-shell.bash
+++ powerline-shell.bash
@@ -25,41 +25,47 @@ __ps_main() {
 
     local -r branch=$($git_en symbolic-ref --short HEAD 2>/dev/null || $git_en describe --tags --always 2>/dev/null)
 
-    [[ -n "$branch" ]] || return
+    [[ -n "${branch}" ]] || return
 
     local -r git_status=$($git_en status --porcelain)
-    if [[ -z "$git_status" ]]; then
-      local status_indicators="$__ps_clean_indicator"
+    if [[ -z "${git_status}" ]]; then
+      local status_indicators="${__ps_clean_indicator}"
     else
-      local status_indicators="$__ps_dirty_indicator"
+      local status_indicators="${__ps_dirty_indicator}"
     fi
 
-    local -r staged_files=$(echo "$git_status" | grep -cE '^[A-Z]' | grep -Eo '\d+')
-    [[ $staged_files -gt 0 ]] 2>/dev/null && status_indicators+=" $__ps_staged_indicator$staged_files"
-    local -r unstaged_files=$(echo "$git_status" | grep -cE '^.[A-Z]' | grep -Eo '\d+')
-    [[ $unstaged_files -gt 0 ]] 2>/dev/null && status_indicators+=" $__ps_unstaged_indicator$unstaged_files"
-    local -r stash_items=$($git_en stash list | wc -l | grep -Eo '\d+')
-    [[ $stash_items -gt 0 ]] 2>/dev/null && status_indicators+=" $__ps_stash_indicator$stash_items"
-    local -r untracked_files=$(echo "$git_status" | grep -cE '^\?\?' | grep -Eo '\d+')
-    [[ $untracked_files -gt 0 ]] 2>/dev/null && status_indicators+=" $__ps_untracked_indicator$untracked_files"
-    local -r git_status_branch=$($git_en status --porcelain --branch)
-    local -r commits_ahead=$(echo "$git_status_branch" | grep -E '^##' | grep -Eo 'ahead \d+' | grep -Eo '\d+')
-    [[ -n "$commits_ahead" ]] && status_indicators+=" $__ps_push_indicator$commits_ahead"
-    local -r commits_behind=$(echo "$git_status_branch" | grep -E '^##' | grep -Eo 'behind \d+' | grep -Eo '\d+')
-    [[ -n "$commits_behind" ]] && status_indicators+=" $__ps_pull_indicator$commits_behind"
+    local -r staged_files=$(echo "${git_status}" | grep -cE '^[A-Z]' | grep -Eo '\d+')
+    [[ "${staged_files}" -gt 0 ]] 2>/dev/null && status_indicators+=" ${__ps_staged_indicator}${staged_files}"
 
-    printf '%s  %s %s ' "$__ps_branch_indicator"  "$branch" "$status_indicators"
+    local -r unstaged_files=$(echo "${git_status}" | grep -cE '^.[A-Z]' | grep -Eo '\d+')
+    [[ "${unstaged_files}" -gt 0 ]] 2>/dev/null && status_indicators+=" ${__ps_unstaged_indicator}${unstaged_files}"
+
+    local -r stash_items=$($git_en stash list | wc -l | grep -Eo '\d+')
+    [[ "${stash_items}" -gt 0 ]] 2>/dev/null && status_indicators+=" ${__ps_stash_indicator}${stash_items}"
+
+    local -r untracked_files=$(echo "${git_status}" | grep -cE '^\?\?' | grep -Eo '\d+')
+    [[ "${untracked_files}" -gt 0 ]] 2>/dev/null && status_indicators+=" ${__ps_untracked_indicator}${untracked_files}"
+
+    local -r git_status_branch=$($git_en status --porcelain --branch)
+
+    local -r commits_ahead=$(echo "${git_status_branch}" | grep -E '^##' | grep -Eo 'ahead \d+' | grep -Eo '\d+')
+    [[ -n "${commits_ahead}" ]] && status_indicators+=" ${__ps_push_indicator}${commits_ahead}"
+
+    local -r commits_behind=$(echo "${git_status_branch}" | grep -E '^##' | grep -Eo 'behind \d+' | grep -Eo '\d+')
+    [[ -n "${commits_behind}" ]] && status_indicators+=" ${__ps_pull_indicator}${commits_behind}"
+
+    printf '%s  %s %s ' "${__ps_branch_indicator}"  "${branch}" "${status_indicators}"
   }
 
   __ps_ps1() {
     # shellcheck disable=SC2181
     if [[ $? -eq 0 ]]; then
-      PS1="$__ps_color_bg_exit_zero "
+      PS1="${__ps_color_bg_exit_zero} "
     else
-      PS1="$__ps_color_bg_exit_nonzero "
+      PS1="${__ps_color_bg_exit_nonzero} "
     fi
 
-    PS1+="$__ps_color_reset$__ps_color_bg_base$__ps_color_fg_base "
+    PS1+="${__ps_color_reset}${__ps_color_bg_base}${__ps_color_fg_base} "
 
     local -r prefix=$($git_en rev-parse --show-prefix 2>/dev/null)
     local -r toplevel=$($git_en rev-parse --show-toplevel 2>/dev/null)
@@ -72,7 +78,7 @@ __ps_main() {
       PS1+="\\w"
     fi
 
-    PS1+=" $__ps_color_reset$__ps_color_bg_git$__ps_color_fg_base"
+    PS1+=" ${__ps_color_reset}${__ps_color_bg_git}${__ps_color_fg_base}"
 
     # Description: https://github.com/njhartwell/pw3nage
     # Related fix in git-bash: https://github.com/git/git/blob/9d77b0405ce6b471cb5ce3a904368fc25e55643d/contrib/completion/git-prompt.sh#L324
@@ -83,10 +89,10 @@ __ps_main() {
       PS1+="$(__ps_repository_status)"
     fi
 
-    PS1+="$__ps_color_reset\\nλ "
+    PS1+="${__ps_color_reset}\\nλ "
   }
 
-  PROMPT_COMMAND="__ps_ps1; $PROMPT_COMMAND"
+  PROMPT_COMMAND="__ps_ps1; ${PROMPT_COMMAND}"
 }
 
 __ps_main && unset -f __ps_main
